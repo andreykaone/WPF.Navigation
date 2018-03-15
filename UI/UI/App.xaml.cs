fe37226d8b7b3a9bd2c39bba69645d1c10b2053c
Кормилицын Андрей Владиславович <andreyka.one.two@gmail.com>
2018-03-15 06:06:51 +0300

    start creating infrastructure

--- UI/UI/App.xaml.cs
+++ UI/UI/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace UI
 {
@@ -13,5 +7,14 @@ namespace UI
     /// </summary>
     public partial class App : Application
     {
+
+        public int Sooqa;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Sooqa = 5;
+        }
     }
 }
---
+++ UI/UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace UI
+{
+    class MainViewModel : BaseViewModel
+    {
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (_id == value)
+                    return;
+
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+        }
+    }
+}
